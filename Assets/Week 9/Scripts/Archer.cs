f3d6ef5e5c5eadd416de6db756c42fb698ebadef
Chanmin <parchanm@sheridancollege.ca>
2024-03-13 10:50:55 -0400

    Task 2 complete chests open

--- Assets/Week 9/Scripts/Archer.cs
+++ Assets/Week 9/Scripts/Archer.cs
@@ -7,6 +7,11 @@ public class Archer : Villager
     public GameObject arrowPrefab;
     public Transform spawnPoint;
 
+    public override ChestType CanOpen()
+    {
+        return ChestType.Archer;
+    }
+
     protected override void Attack()
     {
         destination = transform.position;
